f81bcc2699841c58fac5eafc05c8f81da7ba1c28
amishipatidar <amishipatidar@gmail.com>
2026-03-02 19:54:44 +0530

    Completed

--- singleton-metrics/src/com/example/metrics/MetricsRegistry.java
+++ singleton-metrics/src/com/example/metrics/MetricsRegistry.java
@@ -25,18 +25,28 @@ public class MetricsRegistry implements Serializable {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    private static MetricsRegistry INSTANCE; // BROKEN: not volatile, not thread-safe
+    private static volatile MetricsRegistry INSTANCE; // BROKEN: not volatile, not thread-safe
     private final Map<String, Long> counters = new HashMap<>();
 
+    // Flag to block reflection
+    private static boolean instanceCreated = false;
+
     // BROKEN: should be private and should prevent second construction
-    public MetricsRegistry() {
-        // intentionally empty
+    private MetricsRegistry() {
+        if (instanceCreated) {
+            throw new RuntimeException("Use getInstance()! Singleton already created.");
+        }
+        instanceCreated = true;
     }
 
     // BROKEN: racy lazy init; two threads can create two instances
     public static MetricsRegistry getInstance() {
         if (INSTANCE == null) {
-            INSTANCE = new MetricsRegistry();
+             synchronized (MetricsRegistry.class){
+                if (INSTANCE == null){
+                    INSTANCE = new MetricsRegistry();
+                }
+            }
         }
         return INSTANCE;
     }
@@ -58,4 +68,8 @@ public class MetricsRegistry implements Serializable {
     }
 
     // TODO: implement readResolve() to preserve singleton on deserialization
+    @Serial
+    private Object readResolve() {
+        return getInstance();
+    }
 }
